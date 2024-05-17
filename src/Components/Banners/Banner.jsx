d88ee7961850b8d5d89eb7891d74bba406288325
Joe Madds <115760696+MadHapps@users.noreply.github.com>
2024-05-16 21:57:07 -0700

    Banner file formatedd

--- src/Components/Banners/Banner.jsx
+++ src/Components/Banners/Banner.jsx
@@ -8,23 +8,24 @@ import {
 } from "react-icons/fa6";
 
 export default function Banner({ condition, title, children }) {
+
   const icon = () => {
     if (condition === "success") {
-      return <FaCircleCheck className="banner-icon" />;
-    }
-    if (condition === "warning") {
-      return <FaCircleExclamation className="banner-icon" />;
-    }
-    if (condition === "error") {
-      return <FaCircleXmark className="banner-icon" />;
-    }
-    if (condition === "neutral" || condition === "") {
-      return <FaCircleInfo className="banner-icon" />;
-    }
-  };
+        return <FaCircleCheck className="banner-icon" />;
+      }
+      if (condition === "warning") {
+        return <FaCircleExclamation className="banner-icon" />;
+      }
+      if (condition === "error") {
+        return <FaCircleXmark className="banner-icon" />;
+      }
+      if (condition === "neutral" || condition === "") {
+        return <FaCircleInfo className="banner-icon" />;
+      }
+  }
 
   return (
-    <div className={`banner ${condition} ${children && "row-gap"}`}>
+    <div className={`banner ${condition} ${children && 'row-gap'}`}>
       {icon()}
       <h1 className="banner-title">{title}</h1>
       {children && <p className="banner-description">{children}</p>}
